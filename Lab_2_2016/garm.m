1805a741ed39c10a14ab2efa88fb4a07b6f4a9ac
rch_9 <enlofpoliteh121@gmail.com>
2016-09-26 22:28:19 +0300

    added N

--- Lab_2_2016/garm.m
+++ Lab_2_2016/garm.m
@@ -4,19 +4,27 @@ clear all;
 close all;
 
 clc;
-N=512; %Количество отсчетов (элементов массива y(t))
+N=1024;% %Количество отсчетов (элементов массива y(t))
 K=60; %Количество членов ряда Фурье
 T=pi; %диапазон изменения функции f(i) равен +/-T
 kp=4.1; %количество периодов гармонической функции
 
-CKO_proc=zeros(1,K);
+% CKO_procK=zeros(1,K);
+% for i=1:K
+%     [f, Sa, Sb, Sa0, Sab, y, CKO_procK(i)] = myfunc(N, i, T, kp);
+% end    
+% 
+% i=1:K;
+% plot(i, CKO_procK);
 
-for i=1:K
-    [f, Sa, Sb, Sa0, Sab, y, CKO_proc(i)] = myfunc(N, i, T, kp);
-end    
+K=60;
+CKO_procN=zeros(1,N);
+for i=1:N
+    [f, Sa, Sb, Sa0, Sab, y, CKO_procN(i)] = myfunc(i, K, T, kp);
+end
 
-i=1:K;
-plot(i, CKO_proc);
+i=1:N;
+plot(i, CKO_procN);
 
 %mygraph(N, K, Sa, Sab, f, y);
 
