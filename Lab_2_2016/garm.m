2f4234def7d4fd90216880a6be88857d95d8b5e1
rch9 <enlofpoliteh121@gmail.com>
2016-10-03 22:32:00 +0300

    working garm with old comp

--- Lab_2_2016/garm.m
+++ Lab_2_2016/garm.m
@@ -11,22 +11,24 @@ kp=4.3; %количество периодов гармонической функции
 %взять для 4.5
 
 % Ищем зависимость K
-% CKO_procK=zeros(1,K);
-% for i=1:K
-%     [f, Sa, Sb, Sa0, Sab, y, CKO_procK(i)] = myfunc(N, i, T, kp);
-% end    
-% i=1:K;
-% plot(i, CKO_procK);
+CKO_procK=zeros(1,K);
+for i=1:K
+    [f, Sa, Sb, Sa0, Sab, y, CKO_procK(i)] = myfunc(N, i, T, kp);
+end    
+i=1:K;
+figure
+plot(i, CKO_procK);
 %
 
 % ищем зависимость N
-% K=60;
-% CKO_procN=zeros(1,4);
-% par = [128 256 512 1024];
-% for i=1:4    
-%     [f, Sa, Sb, Sa0, Sab, y, CKO_procN(i)] = myfunc(par(i), K, T, kp);
-% end
-% plot(par, CKO_procN);
+K=60;
+CKO_procN=zeros(1,4);
+par = [128 256 512 1024];
+for i=1:4    
+    [f, Sa, Sb, Sa0, Sab, y, CKO_procN(i)] = myfunc(par(i), K, T, kp);
+end
+figure
+plot(par, CKO_procN);
 %
 
 T=pi;%Значение T (произвольное)
@@ -35,23 +37,25 @@ M=60;%количество членов ряда Фурье
 %p=1;%показатель степени функции x^p 
 kp=4.5;%количество периодов гармонического сигнала
 
-% SKO_procM=zeros(1,M);
-% for i=1:M
-%     [f, C0, C, Cab, y, SKO_procM(i)] = myfunc_comp(N, i+6, T, kp);
-% end
-% i=1:M;
-% plot(i, SKO_procM);
+SKO_procM=zeros(1,M);
+for i=1:M
+    [f, C0, C, Cab, y, SKO_procM(i)] = myfunc_comp(N, i+6, T, kp);
+end
+i=1:M;
+figure
+plot(i, SKO_procM);
 
 M = 32;
 N = 4;
 T  = 4;
-SKO_procM=zeros(1,4);
+SKO_procN=zeros(1,4);
 par = [128 256 512 1024];
 for i=1:4
-    [f, C0, C, Cab, y, SKO_procM(i)] = myfunc_comp (par(i), M, T, kp);
+    [f, C0, C, Cab, y, SKO_procN(i)] = myfunc_comp (par(i), M, T, kp);
 end
 i=1:4;
-plot(par, SKO_procM);
+figure
+plot(par, SKO_procN);
 
 %mygraph(N, K, Sa, Sab, f, y);
 
